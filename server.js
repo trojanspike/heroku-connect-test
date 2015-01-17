22db38513f0561161966851fe9c52c6273d9c120
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-17 17:39:37 +0000

    view folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
 app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname+'/views');
+app.set('views', '/views');
 app.use(express.static(process.cwd() + '/public'));
 
 app.get('/' , function(req, res){
