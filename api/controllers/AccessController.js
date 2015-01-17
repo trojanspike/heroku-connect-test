36b24079126a4a39f09ac953e17dd15530d4620f
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-17 20:10:30 +0000

    access model

---
+++ api/controllers/AccessController.js
@@ -0,0 +1,11 @@
+/**
+ * AccessController
+ *
+ * @description :: Server-side logic for managing accesses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
