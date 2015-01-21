0d179e0f336091eaf533bc1eb2ccdd6d7ed60cdb
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-21 16:36:00 +0000

    remote DBb access

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,9 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+  MongodbServer: {
+    adapter: 'sails-mongo', //
+    url : process.env.MONGO_URL
   },
 
   /***************************************************************************
@@ -82,7 +78,7 @@ module.exports.connections = {
     database: 'YOUR_POSTGRES_DB'
   }
 
-
+// mongodb://trojan:password@ds031329.mongolab.com:31329/mongo-sails
   /***************************************************************************
   *                                                                          *
   * More adapters: https://github.com/balderdashy/sails                      *
