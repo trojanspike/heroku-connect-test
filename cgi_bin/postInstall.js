01f8771be2da9b7abdda0209687c2e54e2228b7c
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-25 16:17:50 +0000

    try without dote for folder

--- cgi_bin/postInstall.js
+++ cgi_bin/postInstall.js
@@ -5,11 +5,11 @@
 var fs = require('fs'),
 exec = require('child_process').exec;
 
-var Bowerrc = fs.readFileSync('./.bowerrc', {encoding:'utf8'}),
+var Bowerrc = fs.readFileSync('.bowerrc', {encoding:'utf8'}),
 BowerDir = JSON.parse(Bowerrc).directory;
-exec('./node_modules/bower/bin/bower install', function(error, stdout, stderr){
+exec('node_modules/bower/bin/bower install', function(error, stdout, stderr){
 
-	exec('rsync -avz '+BowerDir+'/* ./assets/js/00-components/' , function(error, stdout, stderr){
+	exec('rsync -avz '+BowerDir+'/* assets/js/00-components/' , function(error, stdout, stderr){
 
 		exec('rm -Rf '+BowerDir , function(error, stdout, stderr){
 			console.log('Complete cgi');
