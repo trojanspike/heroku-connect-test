2969806e623a9729524f5303a58c42f2e07b40e0
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-25 15:32:53 +0000

    put components into JS folder

--- cgi-bin/postInstall.js
+++ cgi-bin/postInstall.js
@@ -6,4 +6,4 @@ exec = require('child_process').exec;
 var Bowerrc = fs.readFileSync('./.bowerrc', {encoding:'utf8'}),
 BowerDir = JSON.parse(Bowerrc).directory;
 
-exec(' rsync -avz '+BowerDir+'/* ./PUT/ && rm -Rf '+BowerDir)
+exec(' rsync -avz '+BowerDir+'/* ./assets/js/00-components/ && rm -Rf '+BowerDir)
