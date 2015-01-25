076b3880a898d436f8c142821e99f96c573f27b6
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-01-25 16:06:36 +0000

    cgi postInstall in one

--- cgi-bin/postInstall.js
+++ cgi-bin/postInstall.js
@@ -1,9 +1,21 @@
 #!/bin/env node
 
+//- ./node_modules/bower/bin/bower install
+
 var fs = require('fs'),
 exec = require('child_process').exec;
 
 var Bowerrc = fs.readFileSync('./.bowerrc', {encoding:'utf8'}),
 BowerDir = JSON.parse(Bowerrc).directory;
+exec('./node_modules/bower/bin/bower install', function(error, stdout, stderr){
 
-exec(' rsync -avz '+BowerDir+'/* ./assets/js/00-components/ && rm -Rf '+BowerDir)
+	exec('rsync -avz '+BowerDir+'/* ./assets/js/00-components/' , function(error, stdout, stderr){
+
+		exec('rm -Rf '+BowerDir , function(error, stdout, stderr){
+			console.log('Complete cgi');
+		});
+
+	});
+
+});
+
